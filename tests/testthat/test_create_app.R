cbb83df24c6277a376f6413310aa8c63135a88a0
Jonathan Hill <hill@ficonsulting.com>
2017-03-02 21:41:28 -0500

    expect errors

--- tests/testthat/test_create_app.R
+++ tests/testthat/test_create_app.R
@@ -1,6 +1,6 @@
 test_that("create_app runs without errors.", {
-  expect_is(try(create_app("myapp", app_dir = 1)), "try-error")
-  expect_is(try(create_app("myapp", app_dir = tempdir(), dir_out = 1)), "try-error")
-  expect_is(try(create_app("myapp", app_dir = tempdir(), include_R = 1)), "try-error")
-  expect_is(try(create_app("myapp", app_dir = tempdir(), R_version = 1)), "try-error")
+  expect_error(create_app("myapp", app_dir = 1))
+  expect_error(create_app("myapp", app_dir = tempdir(), dir_out = 1))
+  expect_error(create_app("myapp", app_dir = tempdir(), include_R = 1))
+  expect_error(create_app("myapp", app_dir = tempdir(), R_version = 1))
 })
