aad7df70ace201e74ab1263ad6a248661ef6537f
Jonathan Hill <hill@ficonsulting.com>
2017-03-02 21:35:21 -0500

    updated tests

--- tests/testthat/test_create_app.R
+++ tests/testthat/test_create_app.R
@@ -1,13 +1,6 @@
 test_that("create_app runs without errors.", {
-  v <- try(create_app(1))
-  w <- try(create_app("myapp", app_dir = 1))
-  x <- try(create_app("myapp", app_dir = tempdir(), dir_out = 1))
-  y <- try(create_app("myapp", app_dir = tempdir(), include_R = 1))
-  z <- try(create_app("myapp", app_dir = tempdir(), R_version = 1))
-
-  expect_is(v, "try-error")
-  expect_is(w, "try-error")
-  expect_is(x, "try-error")
-  expect_is(y, "try-error")
-  expect_is(z, "try-error")
+  expect_is(try(create_app("myapp", app_dir = 1)), "try-error")
+  expect_is(try(create_app("myapp", app_dir = tempdir(), dir_out = 1)), "try-error")
+  expect_is(try(create_app("myapp", app_dir = tempdir(), include_R = 1)), "try-error")
+  expect_is(try(create_app("myapp", app_dir = tempdir(), R_version = 1)), "try-error")
 })
