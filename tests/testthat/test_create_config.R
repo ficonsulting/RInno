09c03642e01715215606ee10cf69810db3e67300
Jonathan Hill <hill@ficonsulting.com>
2017-03-02 22:01:45 -0500

    syntax is a bitch

--- tests/testthat/test_create_config.R
+++ tests/testthat/test_create_config.R
@@ -1,4 +1,4 @@
-test_that("create_config builds a valid json file." {
+test_that("create_config builds a valid json file.", {
   create_config("myapp", "3.3.2", tempdir())
   config <- fromJSON(file.path(tempdir(), 'config.cfg'))
 
