2e3e43ade9b778e0064718a27876b613000dc297
Jonathan Hill <hill@ficonsulting.com>
2017-03-24 19:25:09 -0400

    minor updates to install_inno

--- R/install_inno.R
+++ R/install_inno.R
@@ -12,6 +12,7 @@
 #' @author Tal Galili and Jonathan M. Hill
 #'
 #' @param quick_start_pack The Inno Setup QuickStart Pack includes Inno Setup and Inno Script Studio script editor. See \href{http://www.jrsoftware.org/is3rdparty.php}{Third-Party Files} page for more information.
+#' @param encryption_module TRUE/FALSE. By default Inno Setup does not include its encryption module, but it is required to add a password to an installer via \code{inst_pw}. Defaults to TRUE.
 #'
 #' @param ... extra parameters to pass to \code{\link[installr]{install.URL}}
 #'
@@ -25,28 +26,30 @@
 #' @export
 
 install_inno <- function(
-  quick_start_pack = FALSE,
+  quick_start_pack = FALSE, encryption_module = TRUE,
   ...) {
 
-  page_with_download_url <- "http://www.jrsoftware.org/download.php/"
-  exe_filename <- "is.exe"
+  page_with_download_url = 'http://www.jrsoftware.org/download.php/'
+  exe_filename = 'is.exe'
 
   if (quick_start_pack) {
-    exe_filename <- "ispack.exe"
+    exe_filename = 'ispack.exe'
   }
 
   URL <- paste0(page_with_download_url, exe_filename)
 
   installr::install.URL(URL, ...)
 
-  # Find the Inno Setup folder
-  progs <- c(list.dirs("C:/Program Files", T, F),
-             list.dirs("C:/Program Files (x86)", T, F))
+  if (encryption_module) {
+    # Find the Inno Setup folder
+    progs <- c(list.dirs("C:/Program Files", T, F),
+               list.dirs("C:/Program Files (x86)", T, F))
 
-  inno <- progs[grep("Inno Setup", progs)]
+    inno <- progs[grep("Inno Setup", progs)]
 
-  # Create a temp file and add the encryption module
-  temp <- tempfile()
-  utils::download.file("http://www.jrsoftware.org/download.php/iscrypt.dll", temp)
-  file.copy(temp, file.path(inno, "ISCrypt.dll"))
+    # Create a temp file and add the encryption module
+    temp <- tempfile()
+    utils::download.file("http://www.jrsoftware.org/download.php/iscrypt.dll", temp)
+    file.copy(temp, file.path(inno, "ISCrypt.dll"))
+  }
 }
