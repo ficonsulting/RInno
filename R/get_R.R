21d59f47b4fbff4397da0fcceddbd6f7528678cd
Jonathan Hill <hill@ficonsulting.com>
2017-10-26 22:11:07 -0400

    updated error message for download functions

--- R/get_R.R
+++ R/get_R.R
@@ -36,7 +36,17 @@ get_R <- function(app_dir, R_version) {
 
     tryCatch(curl::curl_download(base_url, filename),
       error = function(e) {
-      cat(sprintf("\n%s is not a valid URL. \nThis is likely to have happened because there was a change in the URL of the installer. \n\nThis might have already been fixed in the latest version of RInno. Install the latest version using devtools::install_github('ficonsulting/RInno') and try again.\n\nIf this doesn't help please submit an issue, and let us know this function needs updating/fixing (please include the output of sessionInfo() ) - thanks!\n", base_url))
+        cat(glue::glue("
+                              {base_url} is not a valid URL.
+
+                              This is likely to have happened because there was a change in the URL.
+
+                              This might have already been fixed in the latest version of RInno. Install it with devtools::install_github('ficonsulting/RInno').
+
+                              If this doesn't help please submit an issue: {packageDescription('RInno', fields = 'BugReports')}
+
+- Thanks!
+  "))
   })
 
   if (!file.exists(filename)) stop(sprintf("%s failed to download.", filename))
