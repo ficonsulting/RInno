8626c92dc8d3cddf0675936dad1de8181c83d42e
Jonathan Hill <hill@ficonsulting.com>
2018-02-06 15:36:56 -0500

    use httr instead of ping

--- inst/installation/get_app_from_app_url.R
+++ inst/installation/get_app_from_app_url.R
@@ -4,7 +4,7 @@ get_remote_version <- function(
 
   # Fail early
   if (app_repo == "none") return("none")
-  if (!ping_site("www.google.com")) return("none")
+  if (!httr::http_error("www.google.com")) return("none")
 
   # Method to provide more specific feedback
   warn_user <- function(response) {
