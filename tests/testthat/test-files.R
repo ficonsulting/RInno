21b7789fe158b2aeae209d71d137ae4d171964e6
Mikolaj Rybinski <mikolaj.rybinski@id.ethz.ch>
2018-09-13 13:19:35 +0200

    fix files_section test

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -2,5 +2,5 @@ context("files section")
 
 test_that("files returns a character vector.", {
   app_dir <- tempdir()
-  expect_is(files_section("iss", "myapp", dirname(app_dir)), "character")
+  expect_is(files_section("iss", "myapp", dirname(app_dir), "ie"), "character")
 })
