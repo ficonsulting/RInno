561bfef183646f7c8da89216c55d3c132cc93dd8
Jonathan M. Hill <jon.mark.hill@gmail.com>
2018-09-18 16:51:55 -0400

    simplify the structure of nativefier options (use a character string)

--- R/nativefier_app.R
+++ R/nativefier_app.R
@@ -1,7 +1,9 @@
 #' Package app into electron with nativefier
-#'
+#' @inheritParams create_app
 #' @export
-nativefier_app <- function(app_name, app_dir, ...) {
+nativefier_app <- function(app_name, app_dir, nativefier_opts) {
+  cat("\nBuilding stand-alone UI with Electron...\n")
+
   # Get Nodejs, npm and nativefier
   if (!node_exists()) {
     cat("\nNodejs is not installed...\n")
@@ -20,29 +22,14 @@ nativefier_app <- function(app_name, app_dir, ...) {
 
   nativefier_loc <- "nativefier-app"
   local_url <- "http://127.0.0.1:1984/"
-  opts_str = create_opts_str(...)
+  opts_str <- paste(nativefier_opts, collapse = " ")
+
   cmd <- glue::glue(
    "nativefier --name {glue::double_quote(app_name)} {opts_str} {glue::double_quote(local_url)} {glue::double_quote(nativefier_loc)}"
   )
-  #cat(cmd,"\n")
+  cat("\n", cmd, "\n")
   system(cmd)
   setwd(oldwd)
 }
 
-#' Create options string from keyword arguments
-create_opts_str <- function(...) {
-  opts_str <- ""
-  opts <- list(...)
-  for (i in seq_along(opts)) {
-    opt <- opts[i]
-    opt_name <- names(opt)
-    if (is.null(opt_name)) stop("Missing option name")
-    # no need to check if nchar(opt_name) < 1 - "" as a list key is not allowed
-    opt_pref <- if (nchar(opt_name) == 1) "-" else "--"
-    opt_value <- opt[[TRUE]]
-    opt_value <- if (is.null(opt_value) || is.na(opt_value)) "" else
-      glue::glue(" {glue::double_quote(opt_value)}") # always quote values
-    opts_str <- glue::glue("{opts_str} {opt_pref}{opt_name}{opt_value}")
-  }
-  return(opts_str)
-}
+
