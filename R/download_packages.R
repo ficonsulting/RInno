f1b0cc584828d76d78b9fdbcd05abd0f03f43715
Jonathan M. Hill <jon.mark.hill@gmail.com>
2018-09-19 00:21:04 -0400

    remove github internals and use copied remotes code

--- R/download_packages.R
+++ R/download_packages.R
@@ -39,21 +39,22 @@ download_packages <- function(app_dir, pkgs_path, pkgs, repo, remotes, auth_user
 
   # Figure out which files need to be downloaded
   pkgs_files <- lapply(all_deps_no_base, function(x) list.files(pkgs_path, pattern = paste0("^", x, "_")))
-  downloaded_deps <- unlist(lapply(pkgs_files, function(x) if(length(x) >= 1) TRUE else FALSE))
-  using <- pkgs_files[downloaded_deps]
+  downloaded_cran_deps <- unlist(lapply(pkgs_files, function(x) if(length(x) >= 1) TRUE else FALSE))
+  using_cran <- pkgs_files[downloaded_cran_deps]
 
   # Let developer know which files are already included
-  if (length(using) > 0) {
+  if (length(using_cran) > 0) {
     cat("\nUsing packages:\n - ")
-    cat(file.path(pkgs_path, sort(unlist(using))), sep = "\n - ")
+    cat(file.path(pkgs_path, sort(unlist(using_cran))), sep = "\n - ")
   }
 
   # Download any required pacakges in app_dir/pkg_path
-  req_deps <- all_deps_no_base[!downloaded_deps]
-  if (length(req_deps) > 0) {
+  req_cran_deps <- all_deps_no_base[!downloaded_cran_deps]
+  if (length(req_cran_deps) > 0) {
     cat("\nDownloading required packages...\n")
-    utils::download.packages(req_deps, destdir = pkgs_path, repos = repo, type = "win.binary", quiet = TRUE)
+    utils::download.packages(req_cran_deps, destdir = pkgs_path, repos = repo, type = "win.binary")
   }
+  # Validate downloaded packages
   check_pkg_version(pkgs_path, repo)
 
   if (remotes[1] != "none") {
@@ -61,29 +62,29 @@ download_packages <- function(app_dir, pkgs_path, pkgs, repo, remotes, auth_user
     pkgs_files <- lapply(remotes, function(x) {
       list.files(pkgs_path, pattern = gsub("[@#].*", "", basename(x)))
     })
-    downloaded_deps <- unlist(lapply(pkgs_files, function(x) if(length(x) >= 1) TRUE else FALSE))
-    using <- pkgs_files[downloaded_deps]
+    downloaded_remote_deps <- unlist(lapply(pkgs_files, function(x) if(length(x) >= 1) TRUE else FALSE))
+    using_remotes <- pkgs_files[downloaded_remote_deps]
 
-    if (length(using) > 0) {
+    if (length(using_remotes) > 0) {
       cat("Using dowloaded remotes:\n - ")
-      cat(file.path(pkgs_path, using), sep = "\n - ")
+      cat(file.path(pkgs_path, using_remotes), sep = "\n - ")
     }
 
-    req_deps <- remotes[!downloaded_deps]
-    if (length(req_deps) > 0) {
+    req_remote_deps <- remotes[!downloaded_remote_deps]
+    if (length(req_remote_deps) > 0) {
       cat("Downloading remotes... \n - ")
-      cat(remotes, sep = "\n - ")
+      cat(req_remote_deps, sep = "\n - ")
 
-      github_remotes <- lapply(remotes, devtools:::github_remote, username = auth_user,
+      github_remotes <- lapply(req_remote_deps, github_remote, username = auth_user,
                         auth_token = auth_token, host = "https://api.github.com")
 
-      zip_files <- lapply(github_remotes, devtools:::remote_download.github_remote)
+      zip_files <- lapply(github_remotes, remote_download.github_remote)
 
       decompressed_pkgs <- lapply(zip_files, function(x) {
-        gsub("[\\/]+", "/", devtools:::source_pkg(x))
+        gsub("[\\/]+", "/", source_pkg(x))
       })
 
-      lapply(decompressed_pkgs, devtools::build, path = pkgs_path, binary = TRUE)
+      lapply(decompressed_pkgs, pkgbuild::build, dest_path = pkgs_path, binary = TRUE, vignettes = FALSE)
     }
   }
 }
